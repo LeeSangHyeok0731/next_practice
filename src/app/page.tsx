3e80fb26dfaaf696c8423381a384347e9add4461
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-02-20 16:26:41 +0900

    불러온 데이터 화면에 뿌리기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type Data = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
 export default function Home() {
-  return <>Home</>;
+  const [data, setData] = useState<Data[]>([]);
+
+  useEffect(() => {
+    fetch("/api/post")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <>
+      {data.map((x) => (
+        <div key={x.id} style={{ background: "skyblue" }}>
+          <p>{x.id}</p>
+          <p>{x.title}</p>
+          <p>{x.userId}</p>
+        </div>
+      ))}
+    </>
+  );
 }
